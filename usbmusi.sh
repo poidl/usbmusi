7912322d624f8d75686b38c3322cba59319559b8
Stefan <hoitaus@gmail.com>
2017-12-25 09:45:30 +1100

    works

--- usbmusi.sh
+++ usbmusi.sh
@@ -2,43 +2,39 @@
 
 DIR=/home/stefan/Music
 TMP=/tmp/mplayer-control
-errexit() {
+
+function errexit() {
     echo "*MYERROR* "$1 1>&2
     exit 1
 }
 
-# trap ctrl-c and call ctrl_c()
-trap ctrl_c INT
-
 function ctrl_c() {
+        # TODO: only kill the ones started in this script
+        killall mplayer &> /dev/null
         echo "Cleaning up ..."
         rm $TMP 2>/dev/null
         echo "Exiting ... "
         exit 0
 }
 
-rm $TMP 2>/dev/null
-
-
 function process_input() {
     if (( $1==999 )); then
-        exit 0
+        ctrl_c
         # shutdown -h now
     fi
-    if cd $DIR/$NUMBER; then
+    cd $DIR/$1
+    case $? in
+    0)
+        killall mplayer &> /dev/null
         play
-        
+        ;;
+    *)
+        espeak -ven-us+f4 -s170 "Directory does not exist" &> /dev/null
+        ;;
+    esac
 }
 
-while [ 1 ]; do
-
-    read NUMBER
-    until cd $DIR/$NUMBER
-    do
-        espeak -ven-us+f4 -s170 "Directory does not exist" &> /dev/null
-        read NUMBER
-    done
-    process_input $NUMBER
+function play() {
     # handle spaces in list https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
     IFS=$(echo -en "\n")
     LIST=$(find . \( -name '*.flac' -o -name '*.mp3' -o -name '*.wav' \) -print | sort)
@@ -46,7 +42,7 @@ while [ 1 ]; do
     for i in $LIST; do
         echo $i
     done
-    mkfifo $TMP
+    mkfifo $TMP &> /dev/null
     # https://ubuntuforums.org/showthread.php?t=1629000
     # Slave mode commands: http://www.mplayerhq.hu/DOCS/tech/slave.txt
     # why xargs? - can't get it to work otherwise if filenames contain spaces
@@ -54,19 +50,30 @@ while [ 1 ]; do
 
     # https://stackoverflow.com/questions/1570262/shell-get-exit-code-of-background-process
     PID=$!
-    # while   ps | grep " $PID "  | grep -v grep > /dev/null
-    # do
-    #     echo $PID
-    #     sleep 3
-    # done
+    while   ps | grep " $PID "  | grep -v grep > /dev/null
+    do
+        read_input
+    done
 
-    echo "blocking until mplayer exits..."
+    # echo "blocking until mplayer exits..."
     wait $PID
     echo "mplayer exited"
     STATUS=$?
     if [ $STATUS==1 ]; then
         errexit "Error mplayer"
     fi;
+}
 
-done
+function read_input() {
+    while [ 1 ]; do
+        read NUMBER
+        process_input $NUMBER
+    done
+}
+
+# trap ctrl-c and call ctrl_c()
+# https://rimuhosting.com/knowledgebase/linux/misc/trapping-ctrl-c-in-bash
+trap ctrl_c INT
+rm $TMP 2>/dev/null
+read_input
          
